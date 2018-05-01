d57209029c17a219b4020f50d70732430448d274
rococo <rococo@tangleroad.com>
2018-05-01 15:02:30 -0700

    format

--- SummaRuNNer/models/Attention.py
+++ SummaRuNNer/models/Attention.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
+
 class Attention(nn.Module):
     r"""
     Applies an attention mechanism on the query features from the decoder.
@@ -29,6 +30,7 @@ class Attention(nn.Module):
         mask (torch.Tensor, optional): applies a :math:`-inf` to the indices specified in the `Tensor`.
 
     """
+
     def __init__(self):
         super(Attention, self).__init__()
         self.mask = None
@@ -41,11 +43,12 @@ class Attention(nn.Module):
             mask (torch.Tensor): tensor containing indices to be masked
         """
         self.mask = mask
-    
+
     """
         - query   (batch, query_len, dimensions): tensor containing the query features from the decoder.
         - context (batch, input_len, dimensions): tensor containing features of the encoded input sequence.
     """
+
     def forward(self, query, context):
         batch_size = query.size(0)
         dim = query.size(2)
@@ -54,12 +57,13 @@ class Attention(nn.Module):
         attn = torch.bmm(query, context.transpose(1, 2))
         if self.mask is not None:
             attn.data.masked_fill_(self.mask, -float('inf'))
-        attn_scores = F.softmax(attn.view(-1, in_len),dim=1).view(batch_size, -1, in_len)
+        attn_scores = F.softmax(attn.view(-1, in_len), dim=1).view(batch_size, -1, in_len)
 
         # (batch, query_len, in_len) * (batch, in_len, dim) -> (batch, query_len, dim)
         attn_out = torch.bmm(attn_scores, context)
 
         return attn_out, attn_scores
+
 
 if __name__ == '__main__':
     torch.manual_seed(1)
