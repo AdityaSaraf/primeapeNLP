3781170c0b70d108dda91fa0f75ce1a34a7c0c7f
rococo <rococo@tangleroad.com>
2018-05-01 23:12:34 -0700

    fix scripts

--- SummaRuNNer/test_cnn.sh
+++ SummaRuNNer/test_cnn.sh
@@ -1,1 +1,1 @@
- python3.6 main.py -batch_size 1 -test -load_dir checkpoints/RNN_RNN_seed_1.pt -test_dir sample_extracted.json -hyp outputs/cnn_rnn
+ python3.6 main.py -batch_size 1 -test -load_dir checkpoints/CNN_RNN_seed_1.pt -test_dir sample_10k.json -hyp outputs/cnn_rnn10
