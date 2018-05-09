32326fd03b23c272339bdbd617c9dc6f545858e8
rococo <rococo@tangleroad.com>
2018-05-08 20:56:22 -0700

    retokenize

--- error_analysis/retokenize.py
+++ error_analysis/retokenize.py
@@ -15,15 +15,13 @@ def init(dir, target):
             with open(path, "r", encoding="utf-8") as file:
                 out_path = os.path.join(target, filename)
                 with open(out_path, "w", encoding="utf-8") as out:
-                    out_lines = []
                     lines = file.readlines()
                     for line in lines:
                         label = line[0: line.index(" ")]
                         sentence = line[line.index(" ") + 1:]
                         tokens = moses.tokenize(sentence)
-                        out_line = label + " " + " ".join(tokens)
-                        out_lines.append(out_line)
-                    out.writelines(out_lines)
+                        out_line = label + " " + " ".join(tokens) + "\n"
+                        out.write(out_line)
 
 
 if __name__ == "__main__":
